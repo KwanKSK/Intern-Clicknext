e8c987a40605fb6028730a57288ca0471f855911
KSKSIN <77713987+KwanKSK@users.noreply.github.com>
2022-02-15 15:59:00 +0700

    revert string

---
+++ #9.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void RevertString(char string[], int start, int last);
+
+int main() {
+	char str[100];
+    int start = 0;
+	int end = 0;
+	
+	printf("Enter a string to be revert: ");
+	scanf("%[^\n]s", &str);
+
+    while(str[end]) {
+        for(end=start; str[end] && str[end]!=' '; end++);
+        	RevertString(str, start, end-1);
+        start = end + 1;
+    }
+    printf("After the revert of string: %s ", str);
+    
+    return 0;
+}
+
+// user defined function
+void RevertString(char str[], int start, int end) {
+    char tmp;
+    while(start<end) {
+        tmp = str[start];
+        str[start] = str[end];
+        str[end] = tmp;
+        start++;
+        end--;
+    }
+}
